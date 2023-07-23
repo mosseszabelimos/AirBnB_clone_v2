d5aa38802fd0a26c9386c8b0d881b740b93d07a6
Abel <abeldawit.h@gmail.com>
2023-07-23 07:28:00 +0000

    project task updated

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,54 +1,56 @@
 #!/usr/bin/python3
-""" write a script that add fifth view func that displays HTML page if n is int """
-
-from flask import Flask
-from flask import render_template
-
-
+"""
+    Sript that starts a Flask web application
+ """
+from flask import Flask, render_template
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def hello_world():
-    """ Returns some text. """
-    return 'Hello HBNB!'
+@app.route('/', strict_slashes=False)
+def hello_hbn():
+    """
+        function to return Hello HBNB!
+    """
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb')
-def hello():
-    """ Return other text. """
-    return 'HBNB'
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+        function to return HBNB
+    """
+    return "HBNB"
 
 
-@app.route('/c/<text>')
-def c_text(text):
-    """ replace text with variable. """
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
+@app.route('/c/<text>', strict_slashes=False)
+def text_var(text):
+    """
+        function to display text variable passed in
+    """
+    return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python/')
-@app.route('/python/<text>')
-def python_text(text='is cool'):
-    """ replace more text with another variable. """
-    text = text.replace('_', ' ')
-    return 'Python {}'.format(text)
+@app.route('/python/<text>', strict_slashes=False)
+def text_var_python(text="is cool"):
+    """
+        function to display text variable, with default "is cool"
+    """
+    return "Python {}".format(text.replace("_", " "))
 
 
-@app.route('/number/<int:n>')
-def number_text(n):
-    """ replace with int only if given int. """
-    n = str(n)
-    return '{} is a number'.format(n)
+@app.route('/number/<int:n>', strict_slashes=False)
+def var_num(n):
+        """
+             function to display a variable, but only if an int
+        """
+        return "{} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>')
-def html_num(n):
-    """ display html if n is int. """
-    n = str(n)
-    return render_template('5-number.html', n=n)
-
-
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def var_num_template(n):
+        """
+            function to display number in html page
+        """
+        return render_template("5-number.html", n=n)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+        app.run(host='0.0.0.0', port=5000)

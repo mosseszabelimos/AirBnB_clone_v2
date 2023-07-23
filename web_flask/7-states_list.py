5b81bfc386cc3b42273bdc0addb339de0f2c944f
Abel <abeldawit.h@gmail.com>
2023-07-23 07:35:21 +0000

    error fix for task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
-""" 7. Start flask service that does something. """
-
-from flask import Flask
-from flask import render_template
-
-
+"""
+    Sript that starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-#@app.route()
+@app.teardown_appcontext
+def handle_teardown(self):
+    """
+        method to handle teardown
+    """
+    storage.close()
 
+
+@app.route('/states_list', strict_slashes=False)
+def state_list():
+    """
+        method to render states
+    """
+    states = storage.all('State').values()
+    return render_template("7-states_list.html", states=states)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+        app.run(host='0.0.0.0', port=5000)
